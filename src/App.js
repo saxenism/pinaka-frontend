a28a36e149ca7899c0e070c19f0d70b39d57e92d
Rahul Saxena <b417033@iiit-bh.ac.in>
2022-06-05 00:03:09 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './App.css'
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [thankYouTextVisible, setThankYouTextVisible] = useState(false);
+  const [disableEmail, setDisableEmail] = useState(false);
+  const [submitButtonColor, setSubmitButtonColor] = useState('bg-indigo-800');
+  const [submitButtonText, setSubmitButtonText] = useState('Submit');
+
+  function handleSubmit(event) {
+   console.log("handleSubmit started running");   
+   event.preventDefault();
+
+   console.log(email);
+   setEmail('');
+   setSubmitButtonColor('bg-green-400');
+   setSubmitButtonText('Submitted');
+   setThankYouTextVisible(true);
+   setDisableEmail(true);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className ="text-center align-middle">
+      <div className="align-middle my-8 text-gray-100 ">
+        <h1 className='text-3xl align-middle'>Welcome to Pinaka</h1>
+        <h3 class="text-2xl text-center">The web3 enabled evolution of e-commerce</h3>
+        <br />
+        <form 
+          name='chadEmails'
+          method='POST'
+          data-netlify='true'
+          onSubmit={handleSubmit} 
+          className='mb-3'
         >
-          Learn React
-        </a>
-      </header>
+          <label className='text-xl mb-3'>
+            Interested to know more? Join the waitlist
+              <br />
+              <input 
+                id='email' 
+                disabled = {disableEmail}
+                placeholder = 'Enter e-mail' 
+                className = "py-1 text-gray-800 text-center mr-3" 
+                onChange={event => setEmail(event.target.value)}
+                value = {email}
+                type="email" 
+                name='email'
+              />
+              <button 
+                type='submit'
+                className={`py-1.5 px-8 ${submitButtonColor} text-white font-bold`}>
+                  {submitButtonText}
+                </button>
+          </label>
+        </form>
+        {
+          thankYouTextVisible ? 
+            <h3>Thank you Chad. Will follow up soon.</h3>
+          : null
+        }
+      </div>
     </div>
   );
 }
-
 export default App;
