be1b1349f6b7df44131143210b9340fa99d2c0d2
Rahul Saxena <b417033@iiit-bh.ac.in>
2022-06-05 12:24:18 +0530

    Added requested changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
+import { useState } from 'react';
 import './App.css'
 
 function App() {
+
+  const [email, setEmail] = useState('');
+  const [disabled, setDisabled] = useState(false);
+  const [thankYouText, setThankYouText] = useState(false);
+  const [submitButtonText, setSubmitButtonText] = useState('Submit');
+  const [submitButtonColor, setSubmitButtonColor] = useState('bg-indigo-800');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    console.log(email);
+
+    setEmail('');
+    setDisabled(true);
+    setThankYouText(true);
+    setSubmitButtonText('Submitted');
+    setSubmitButtonColor('bg-green-400');
+  }
 
   return (
     <div className ="text-center align-middle">
@@ -12,7 +31,7 @@ function App() {
           name='chadEmails'
           method='post'
           data-netlify="true" 
-          onSubmit="submit" 
+          onSubmit={handleSubmit} 
           className='mb-3'
         >
           <input type="hidden" name="pinaka-Emails" value="chadEmails"/>
@@ -22,17 +41,27 @@ function App() {
               <input 
                 id='chadEmail' 
                 placeholder = 'Enter e-mail' 
+                disabled = {disabled}
                 className = "py-1 text-gray-800 text-center mr-3"
+                onChange={event => setEmail(event.target.value)}
+                value={email}
                 type="email" 
                 name='email'
               />
               <button 
                 type='submit'
-                className={`py-1.5 px-8 bg-indigo-800 text-white font-bold`}>
-                  Submit
+                className={`py-1.5 px-8 ${submitButtonColor} text-white font-bold`}>
+                  {submitButtonText}
                 </button>
           </label>
         </form>
+        {
+          thankYouText ? 
+          <h3 class="text-lg text-center">
+            Thank you for your interest Chad. Will get back to you soon.
+          </h3> :
+          null
+        }
       </div>
     </div>
   );
